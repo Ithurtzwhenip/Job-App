9a8651b9febc09bf071b6a48194f8aeec11c1b70
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-01-18 21:28:29 +0530

    basic html

--- app/views.py
+++ app/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("<h3>Hello World!</h3>")
 
 def job_details(request, id):
-    return HttpResponse(f"This is the job detail page {id}")
+    # return HttpResponse(f"<h1>This is the job detail page {id}</h1>")
+    site = "http://google.com"
 
+    return HttpResponse(f"Visit <a href={site}> Google here </a>")
