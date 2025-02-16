7331c9ebf455768bffeff1387d1ca509e8224a05
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-02-16 22:28:35 +0530

    search

--- app/admin.py
+++ app/admin.py
@@ -6,4 +6,6 @@ from app.models import JobPost
 class JobAdmin(admin.ModelAdmin):
     list_display = ('__str__','title','salary','date')
     list_filter = ('date','salary','expiry',)
+    search_fields = ('title','description')
+    search_help_text = "Write your query and hit enter"
 admin.site.register(JobPost, JobAdmin)
