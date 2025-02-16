717690e2e4e8ff5a015946aa09fce6dff5d81254
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-02-16 22:52:45 +0530

    layout changes

--- app/admin.py
+++ app/admin.py
@@ -8,4 +8,6 @@ class JobAdmin(admin.ModelAdmin):
     list_filter = ('date','salary','expiry',)
     search_fields = ('title','description')
     search_help_text = "Write your query and hit enter"
+    # fields = (('title','description'),'expiry')
+    exclude = ('title',)
 admin.site.register(JobPost, JobAdmin)
