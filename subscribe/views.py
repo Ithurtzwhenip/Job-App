1ab0975847504af08ab48e352d90dfbbc1ccaf4f
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-02-23 16:31:43 +0530

    server side config

--- subscribe/views.py
+++ subscribe/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def subscribe(request):
-    context = {}
+    email_error = ""
+    if request.POST:
+        email = request.POST['email']
+        print("Post Request: ",email)
+        if email =="":
+            email_error = "Please enter a valid email address"
+    context = {"email_error": email_error}
     return render(request, 'subscribe/subscribe.html', context)
