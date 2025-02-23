ef472e72205a61c7320f52b05aaa2e9d96affb4c
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-02-23 16:47:56 +0530

    migrations

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 
+from subscribe.models import Subscribe
+
+
 # Create your views here.
 
 def subscribe(request):
     email_error = ""
     if request.POST:
+        first_name = request.POST['firstname']
+        last_name = request.POST['firstname']
         email = request.POST['email']
         print("Post Request: ",email)
         if email =="":
             email_error = "Please enter a valid email address"
+
+        subscribe = Subscribe(first_name=first_name, last_name=last_name, email=email)
+        subscribe.save()
+
     context = {"email_error": email_error}
     return render(request, 'subscribe/subscribe.html', context)
