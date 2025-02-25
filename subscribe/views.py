4d113f2a9132f33eb2b2d5d1063762a52620027e
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-02-25 14:58:05 +0530

    form update

--- subscribe/views.py
+++ subscribe/views.py
@@ -3,21 +3,12 @@ from subscribe.forms import SubscribeForm
 from subscribe.models import Subscribe
 
 
-# Create your views here.
-
 def subscribe(request):
     subscribe_form = SubscribeForm()
     email_error = ""
     if request.POST:
-        first_name = request.POST['firstname']
-        last_name = request.POST['firstname']
-        email = request.POST['email']
-        print("Post Request: ",email)
-        if email =="":
-            email_error = "Please enter a valid email address"
-
-        subscribe = Subscribe(first_name=first_name, last_name=last_name, email=email)
-        subscribe.save()
-
-    context = {"form":subscribe_form,"email_error": email_error}
+        subscribe_form = SubscribeForm(request.POST)
+        if subscribe_form.is_valid():
+            print("Valid form")
+    context = {"form": subscribe_form, "email_error": email_error}
     return render(request, 'subscribe/subscribe.html', context)
