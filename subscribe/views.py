2e14aa762834e443031e7ed604982c4d4fb618da
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-02-25 14:25:27 +0530

    forms

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from subscribe.forms import SubscribeForm
 from subscribe.models import Subscribe
 
 
 # Create your views here.
 
 def subscribe(request):
+    subscribe_form = SubscribeForm()
     email_error = ""
     if request.POST:
         first_name = request.POST['firstname']
@@ -18,5 +19,5 @@ def subscribe(request):
         subscribe = Subscribe(first_name=first_name, last_name=last_name, email=email)
         subscribe.save()
 
-    context = {"email_error": email_error}
+    context = {"form":subscribe_form,"email_error": email_error}
     return render(request, 'subscribe/subscribe.html', context)
