f5bec7c9a7a5f083414381b115dc9ac0aac65738
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-02-26 16:35:18 +0530

    error messages

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -1,7 +1,34 @@
 from django import forms
+from subscribe.models import Subscribe
+from django.utils.translation import gettext_lazy as _
 
+class SubscribeForm(forms.ModelForm):
+    class Meta:
+        model = Subscribe
+        fields = '__all__'
+        labels = {
+            'first_name':_('Enter First Name'),
+            'last_name':_('Enter Last Name'),
+            'email':_('Enter Email')
+        }
+        help_texts = {'first_name':_("Enter characters only")}
+        error_messages = {
+            'first_name':{
+                'required':_('You must enter a first name')
+            }
+        }
 
-class SubscribeForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=100)
-    last_name = forms.CharField(label='Last Name', max_length=100)
-    email = forms.EmailField(max_length=100)
+# def validate_comma(value):
+#     if ',' in value:
+#         raise forms.ValidationError("Invalid Name")
+#     return value
+#
+#
+# class SubscribeForm(forms.ModelForm):
+#     class Meta:
+#
+#
+#     # first_name = forms.CharField(max_length=100, required=False, label="Enter first name",
+#     #                              )
+#     # last_name = forms.CharField(max_length=100, disabled=False, validators=[validate_comma])
+#     # email = forms.EmailField(max_length=100)
--- subscribe/views.py
+++ subscribe/views.py
@@ -10,14 +10,7 @@ def subscribe(request):
     if request.POST:
         subscribe_form = SubscribeForm(request.POST)
         if subscribe_form.is_valid():
-            print("Valid form")
-            print(subscribe_form.cleaned_data)
-            email = subscribe_form.cleaned_data['email']
-            first_name = subscribe_form.cleaned_data['first_name']
-            last_name = subscribe_form.cleaned_data['last_name']
-            print(email, first_name, last_name)
-            subscribe = Subscribe(email=email, first_name=first_name, last_name=last_name)
-            subscribe.save()
+            subscribe_form.save()
             return redirect(reverse('thank_you'))
     context = {"form": subscribe_form, "email_error": email_error}
     return render(request, 'subscribe/subscribe.html', context)
