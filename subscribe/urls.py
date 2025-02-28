a07ee372e73618204aa3a9b7730862a23e579f36
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-02-28 16:02:09 +0530

    fixes

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('subscribe/', views.subscribe, name='subscribe' ),
-    path('thank_you/', views.thank_you, name='thank_you')
+    path('', views.subscribe, name='subscribe'),  # subscribe/ (default page)
+    path('thank_you/', views.thank_you, name='thank_you'),  # subscribe/thank_you/
 ]
